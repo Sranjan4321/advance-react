f13e2074295b87e578d20443a4cc19dd48d6ae3e
Sranjan4321 <shashi94ranjan@gmail.com>
2022-05-20 00:45:19 +0530

    commit for hooks

--- advance/src/App.tsx
+++ advance/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Country from './assign/Country';
+// import Country from './assign/Country';
+import Githubs from './assign/Hooks/Githubs';
 
 function App() {
   return (
     <div className="App">
-     <Country/>
+     {/* <Country/> */}
+     <Githubs/>
     </div>
   );
 }
