56d361fd7d3bf7056177b9cd6fb3ae69c5870118
Sranjan4321 <shashi94ranjan@gmail.com>
2022-07-12 22:50:11 +0530

    commit

---
+++ advance/src/Costum component/Apps.tsx
@@ -0,0 +1,44 @@
+import "../App.css"
+import { Accordian } from "./Accordian";
+// import { Element } from "./Accordian";
+ import { OtpInput } from "./OtpInput"
+export const handleChang=(Otp:string)=>{
+
+}
+ function Apps(){
+    const data = [
+        {
+          dec:
+            "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+            title: "This is panel header 1",
+        },
+        {
+          dec:
+            "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+          title: "This is panel header 2",
+        },
+        {
+          dec:
+            "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+          title: "This is panel header 3",
+        },
+        {
+          dec:
+            "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+          title: "This is panel header 4",
+        },
+        {
+          dec:
+            "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+          title: "This is panel header 5",
+        },
+      ];
+     return(
+         <div className="app">
+           <OtpInput totalInputs={5}  onChange={handleChang}/>
+           <Accordian data={data}/>
+         </div>
+     )
+ }
+
+ export default Apps
